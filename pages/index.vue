0499207b01fc5df5736a642ed0e523edafd5a5c0
Makoto Chiba <hypermkt@gmail.com>
2018-07-31 21:41:41 +0900

    デザイン調整

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,27 @@
       <v-card>
         <v-toolbar color="cyan" dark>
           <v-toolbar-side-icon></v-toolbar-side-icon>
-          <v-toolbar-title>Tech Articles</v-toolbar-title>
+          <v-toolbar-title>Collective Times</v-toolbar-title>
           <v-spacer></v-spacer>
         </v-toolbar>
 
         <v-list two-line>
-          <template v-for="article in articles">
-            <v-list-tile :key="article.title">
+          <template v-for="(article, index) in articles">
+            <v-list-tile
+                    ripple
+                    :key="article.title"
+                    :href="article.articleUrl"
+            >
+              <v-avatar>
+                <img :src="article.imageUrl">
+              </v-avatar>
               <v-list-tile-content>
                 <v-list-tile-title>{{ article.title }}</v-list-tile-title>
                 <v-list-tile-sub-title>{{ article.description }}</v-list-tile-sub-title>
+                <v-list-tile-sub-title>{{ article.date }}</v-list-tile-sub-title>
               </v-list-tile-content>
             </v-list-tile>
+            <v-divider v-if="index + 1 < articles.length" :key="`divider-${index}`"></v-divider>
           </template>
         </v-list>
       </v-card>
