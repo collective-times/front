d5bdf2513118a33be2ad6fb5c9eb3266be88cf7e
Makoto Chiba <hypermkt@gmail.com>
2018-08-16 14:09:30 +0900

    これが必要なのかな

--- env.production.js
+++ env.production.js
@@ -1,3 +1,5 @@
 module.exports = {
-  apiBaseUrl: 'https://collective-times-api.herokuapp.com'
+  apiBaseUrl: 'https://collective-times-api.herokuapp.com',
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,　
 };
