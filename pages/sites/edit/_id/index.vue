f7b9b7e11d03b6b7debde028f0fcd9341ae55e2f
Makoto Chiba <hypermkt@gmail.com>
2018-08-18 16:00:03 +0900

    削除を実行する前にダイアログで確認させる

--- pages/sites/edit/_id/index.vue
+++ pages/sites/edit/_id/index.vue
@@ -27,10 +27,33 @@
             @click="updateSite($route.params.id)"
     >Submit</v-btn>
 
-    <v-btn
-            color="warning"
+    <v-dialog
+      width="500"
+    >
+      <v-btn
+              color="warning"
+              slot="activator"
+      >Delete</v-btn>
+
+      <v-card>
+        <v-card-text>
+          Are you sure to delete?
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
             @click="deleteSite($route.params.id)"
-    >Delete</v-btn>
+          >
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <v-btn
       @click="$router.push('/sites')"
