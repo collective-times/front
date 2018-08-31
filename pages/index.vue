267d248f778481cbd6704fe946f1a954a8fffbc7
moonstruckdrops <moonstruckdrops@gmail.com>
2018-08-31 18:09:29 +0900

    はてな以外のカラー設定

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,41 @@
   <v-card>
     <v-list two-line>
       <template v-for="(article, index) in articles">
-        <v-list-tile
-                ripple
-                :key="article.title"
-                :href="article.articleUrl"
-                target="_blank"
-        >
-          <v-avatar class="mr-2">
-            <img :src="article.imageUrl">
-          </v-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title v-html="article.title"></v-list-tile-title>
-            <v-list-tile-sub-title v-html="article.description"></v-list-tile-sub-title>
-            <v-list-tile-sub-title v-html="(article.date + ' ' + article.sourceTitle)"></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
+        <div v-if="article.sourceTitle.match(/.*はてな.*/)">
+          <v-list-tile
+                  ripple
+                  :key="article.title"
+                  :href="article.articleUrl"
+                  target="_blank"
+                  color="#d5f9b6"
+          >
+            <v-avatar class="mr-2">
+              <img :src="article.imageUrl">
+            </v-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="article.title"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="article.description"></v-list-tile-sub-title>
+              <v-list-tile-sub-title v-html="(article.date + ' ' + article.sourceTitle)"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </div>
+        <div v-else>
+          <v-list-tile
+                  ripple
+                  :key="article.title"
+                  :href="article.articleUrl"
+                  target="_blank"
+          >
+            <v-avatar class="mr-2">
+              <img :src="article.imageUrl">
+            </v-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="article.title"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="article.description"></v-list-tile-sub-title>
+              <v-list-tile-sub-title v-html="(article.date + ' ' + article.sourceTitle)"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </div>
         <v-divider v-if="index + 1 < articles.length" :key="`divider-${index}`"></v-divider>
       </template>
       <no-ssr>
